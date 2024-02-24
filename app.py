2973f6d4b42ada4696a166add0d0203a83c8fd89
Alvin Li <aflip24@gmail.com>
2024-02-24 04:31:45 -0600

    add working plot of data

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
+plt.switch_backend('WebAgg')
+
+# Load data from csv file
+data = pd.read_csv('amazon.csv')
+
+# Show data
+print(data)
+
+# Convert data to numeric
+data['Close/Last'] = data['Close/Last'].replace('[\$]', '', regex=True).astype(float)
+
+# Plot data
+data['Close/Last'].plot()
+
+plt.show()
