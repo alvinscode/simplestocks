dbf9b1723971bcdf89939efa2750e2b3973902b6
Alvin Li <aflip24@gmail.com>
2024-02-24 05:13:43 -0600

    fix bugs, use data from MarketWatch instead

--- app.py
+++ app.py
@@ -2,18 +2,30 @@ import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
-plt.switch_backend('WebAgg')
-
 # Load data from csv file
-data = pd.read_csv('amazon.csv')
+data = pd.read_csv('amzn.csv')
 
-# Show data
-print(data)
+# Convert 'Volume' column to numeric
+data['Volume'] = data['Volume'].str.replace(',', '').astype(int)
 
-# Convert data to numeric
-data['Close/Last'] = data['Close/Last'].replace('[\$]', '', regex=True).astype(float)
+# Split data into training and testing data sets
+train_data = data.iloc[:int(.99 * len(data)), :]
+test_data = data.iloc[int(0.99 * len(data)):, :]
 
-# Plot data
-data['Close/Last'].plot()
+# Define features and target variable
+features = ['Open', 'Volume']
+target = 'Close'
 
-plt.show()
+# Create and train model
+model = xgb.XGBRegressor()
+model.fit(train_data[features], train_data[target])
+
+# Make and show predictions on test data
+predictions = model.predict(test_data[features])
+print('Model Predictions:')
+print(predictions)
+
+# Show actual values
+
+print('Actual Values:')
+print(test_data[target])
