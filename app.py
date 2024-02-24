ebc6806c269b03b8d2b180d64421c00c00590ec5
Alvin Li <aflip24@gmail.com>
2024-02-24 05:20:58 -0600

    data was prediciting backwards, reversed order of data

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ import pandas as pd
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
+plt.switch_backend('WebAgg')
+
 # Load data from csv file
 data = pd.read_csv('amzn.csv')
 
 # Convert 'Volume' column to numeric
 data['Volume'] = data['Volume'].str.replace(',', '').astype(int)
+
+# Reverse order of data
+
+data = data.iloc[::-1].reset_index(drop=True)
 
 # Split data into training and testing data sets
 train_data = data.iloc[:int(.99 * len(data)), :]
@@ -29,3 +35,15 @@ print(predictions)
 
 print('Actual Values:')
 print(test_data[target])
+
+# Show models accuracy
+accuracy = model.score(test_data[features], test_data[target])
+print('Accuracy:')
+print(accuracy)
+
+# Plot predictions and close price
+
+plt.plot(data['Close'], label='Close Price')
+plt.plot(test_data[target].index, predictions, label='Predictions')
+plt.legend()
+plt.show()
